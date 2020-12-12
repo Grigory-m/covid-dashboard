65489150d9817da96baa2966a3203326d0333770
Grisha <gm201212@gmail.com>
2020-12-12 20:02:30 +0300

    feat: add getting data from api

---
+++ src/js/api/data.js
@@ -0,0 +1,26 @@
+export default class {
+
+  constructor(url) {
+    this.url = url;
+  }
+
+  getFlagsAndPopulations = async () => {
+    try {      
+      const response = await fetch(`${this.url}all?fields=name;flag;population;`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error('Не удалось получить данные о стране!')
+    }    
+  }
+
+  getGlobalCases = async () => {
+    try {
+      let response = await fetch(`${this.url}`);
+      let data = await response.json();
+      return data;
+    } catch (error) {
+      throw new Error('Не удалось получить данные о COVID-19!')
+    }    
+  }
+}

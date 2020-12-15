b1b07a39bce8e7f93f1fefeef1c35b1fec224610
Grigory Murashka <gm201212@gmail.com>
2020-12-15 15:28:00 +0300

    fix: fix list

--- src/js/components/list.js
+++ src/js/components/list.js
@@ -3,6 +3,30 @@ class List {
     this.value = value;
   }
 
+  createListContent = (data, value = 'TotalConfirmed') => {
+    this.createListMarkup();
+    const list = document.querySelector('.table > tbody');
+    const typeOfCases = document.querySelector('.type-case');
+    let dataCovid = data;
+    typeOfCases.innerText = `${value.slice(5)}`;
+    if (!data.Countries) {
+      dataCovid = JSON.parse(localStorage.getItem('data'));
+    }
+    dataCovid.Countries.sort((a, b) => b[value] - a[value]).forEach((country) => {
+      const listPosition = document.createElement('tr');
+      const img = new Image();
+
+      listPosition.classList.add('table-dark');      
+      img.src = `https://www.countryflags.io/${country.CountryCode.toLowerCase()}/flat/64.png`;
+        listPosition.innerHTML = `
+        <td><div class="flag"><img src="${img.src}" alt="flag"></div></td>
+        <th scope="row">${country.Country}</th>
+        <td>${country[value]}</td>
+        `;
+        list.append(listPosition);             
+    })       
+  }
+
   createListMarkup = () => {
     const tabContent = document.getElementById('myTabContent');
 
@@ -16,21 +40,10 @@ class List {
                   <thead>
                     <tr>
                       <th scope="col" colspan="2">Country</th>
-                      <th scope="col">Cases</th>
+                      <th class="type-case" scope="col">Cases</th>
                     </tr>
                   </thead>
-                  <tbody>
-                  <tr class="table-dark">
-                    <td><div class="flag"></div></td>
-                    <th scope="row">Saint Vincent and the Grenadines</th>
-                    <td>Column content</td>
-                  </tr>
-                  <tr class="table-active">
-                    <td><div class="flag"></div></td>
-                    <th scope="row">Active</th>
-                    <td>Column content</td>
-                  </tr>
-                  </tbody>
+                  <tbody></tbody>
                 </table>
               </div>
             </div>
--- src/js/covid.js
+++ src/js/covid.js
@@ -1,5 +1,6 @@
 import Layout from './components/layout';
 import List from './components/list';
+import Data from './api/data';
 
 class Covid {
 
@@ -7,8 +8,23 @@ class Covid {
     const layout = new Layout();
     const list = new List();
 
-    layout.createLayout();
-    list.createListMarkup();
+    layout.createLayout();    
+    this.getData()
+      .then(() => {        
+        list.createListContent(this.data);
+      })
+  }
+
+  getData = async () => {
+    const countries = new Data('https://restcountries.eu/rest/v2/');
+    const covid = new Data('https://api.covid19api.com/summary');
+    
+    this.countries = await countries.getFlagsAndPopulations();
+    this.data = await covid.getGlobalCases();
+    
+    if (this.data.Message === '') {
+      localStorage.setItem('data', JSON.stringify(this.data));
+    }    
   }
 }
 
